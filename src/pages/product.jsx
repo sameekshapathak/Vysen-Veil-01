ce5eaf39dcfa5f3395077d65cdb9d20700cb2c44
sameeksha pathak <sameekshapathak@sameeksha-ka-MacBook-Air.local>
2026-07-24 23:21:07 +0530

    Fix product CSS import path

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Look1 from "../assets/images/Look1.jpg";
-import "./Product.css";
+import "./product.css";
 
 function Product() {
 
